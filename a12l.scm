f470c68b31afc61e114c8fac2334b225a0312c5b
Albin Otterhäll <dev@a12l.xyz>
2022-11-14 03:20:25 +0100

    ripgrep: intro

--- a12l.scm
+++ a12l.scm
@@ -11,7 +11,8 @@
 (home-environment
   ;; Below is the list of packages that will show up in your
   ;; Home profile, under ~/.guix-home/profile.
-  (packages (specifications->packages (list "kakoune")))
+  (packages (specifications->packages (list "kakoune"
+                                            "ripgrep")))
 
   ;; Below is the list of Home services.  To search for available
   ;; services, run 'guix home search KEYWORD' in a terminal.
